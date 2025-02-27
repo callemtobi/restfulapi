20ea1fbd4291c4cf38719213a287113679421017
Fenton <callemtobi@gmail.com>
2025-02-27 23:24:31 +0500

    Routes

--- server.js
+++ server.js
@@ -28,10 +28,69 @@ const wikiSchema = new mongoose.Schema({
 const Article = new mongoose.model('Article', wikiSchema);
 
 // ------------- ROUTES
-app.get('/', (req, res) => {
-    res.send('All good.');
-})
+app.route('/articles')
+    .get((req, res) => {
+        Article.find({})
+        .then((data) => {
+            console.log('----------> Data found!');
+            res.send(data);
+        })
+        .catch((err) => {console.log('----------> Get Error: ' + err)})
 
+    })
+    .post((req, res) => {
+        // Create article
+        let userData = {
+            title: req.body.title,
+            content: req.body.content
+        };
+
+        Article.insertOne(userData)
+        .then(() => {console.log('----------> Success! Created.'); res.send('Created!')})
+        .catch((err) => {console.log('----------> Post Error: ' + err); res.send('Error!')})
+    })
+    // .delete() -> delete all articles
+
+app.route('/articles/:title')
+    .get((req, res) => {                // ----------- GET Request
+        // Get user with Title
+        Article.findOne({title: req.params.title})
+        .then((data) => {
+            if (!data) {
+                console.log('----------> Not found!')
+                res.send('Not found!')
+            } else {
+                res.send(data)
+            }
+        })
+        .catch((err) => console.log('----------> Get Specific Error: ' + err))
+    })
+    .put((req, res) => {               // ----------- PATCH Request
+        // Update article Completely
+        Article.findOneAndUpdate({title: req.params.title}, {title: req.body.title, content: req.body.content})
+        .then((data) => {
+            if (!data) {
+                console.log('----------> Not found!')
+                res.send('Not found!')
+            } else {
+                console.log('----------> Success! Created.'); 
+                res.send('Updated!');
+            }
+        })
+        .catch((err) => {console.log('----------> Put Error: ' + err); res.send('Error!')})
+    })
+    .delete((req, res) => {         // ----------- DELETE Request
+        // Delete specific article
+        Article.findOne({title: req.params.title})
+        .then((data) => {
+            if(!data) {
+                console.log('----------> Not found!'); res.send('Not found!')
+            } 
+            console.log('----------> Success! Deleted.'); 
+            res.send('Deleted!');
+        })
+        .catch((err) => {console.log('----------> Delete Error: ' + err); res.send('Error!')})
+    })
 
 // ------------- PORT
 app.listen(PORT, () => {
